878e6afd2767318c8cc51a253a993d4ed4ce4edb
HoWoong <dlghdnd@gmail.com>
2023-08-18 23:56:25 +0900

    Chapter 3. Using JdbcUserDetailsManager to perform authentication

--- src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -4,7 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -20,5 +30,47 @@ public class ProjectSecurityConfig {
         return http
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults()).build();
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("reader")
+////                .build();
+////
+////        return new InMemoryUserDetailsManager(admin, user);
+//
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User
+//                .withUsername("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
